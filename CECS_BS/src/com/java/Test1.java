48cb83b51195771d0f744712c55f170e9d1cbedf
Kevin <Kevin@Kevins-MacBook-Pro.local>
2015-10-11 22:47:20 -0700

    Buttons, Jlist and Card Display.

--- CECS_BS/src/com/java/Test1.java
+++ CECS_BS/src/com/java/Test1.java
@@ -2,6 +2,7 @@ package com.java;
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 
 public class Test1 {
@@ -16,16 +17,20 @@ public class Test1 {
         JPanel upperPanel = new JPanel();
         JPanel bottomPanel = new JPanel();
         
-        JPanel drawCardPanel = new JPanel();
-        JPanel moveCardPanel = new JPanel();
-        JPanel playCardPanel = new JPanel();
-        
+        JPanel buttonPanel = new JPanel();
+        JPanel JListPanel = new JPanel();
+        List moveList = new List();
+        moveList.add("Brautman Hall");
+        moveList.add("ECS 302");
+
+        JPanel cardPanel = new JPanel();
         
         // Nirupama /c/Nirupama/SEproject/CECS543    SEproject\CECS543\CECS_BS\src
         //Image image = new ImageIcon("C:\\Nirupama\\SEproject\\CECS543\\CECS_BS\\src\\com\\drawable\\CSULBMap3.png").getImage();
         //Kevin  
         Image image = new ImageIcon("/Users/Kevin/Desktop/CSULB/CECS543Project/CECS_BS/src/com/drawable/CSULBMap3.png").getImage();
-        
+        Image card1 = new ImageIcon("/Users/Kevin/Desktop/CSULB/CECS543Project/CECS_BS/src/com/drawable/Card1.png").getImage();
+       
         JButton drawCardButton = new  JButton("Draw Card");
         JButton moveButton = new  JButton("    Move   ");
         JButton playCardButton = new  JButton("Play Card");
@@ -34,6 +39,7 @@ public class Test1 {
         
         
         JLabel jl = new JLabel(new ImageIcon(image));
+        JLabel card1Label = new JLabel(new ImageIcon(card1));
         
         JLabel label1 = new JLabel("Niru");
         label1.setForeground(Color.blue);
@@ -49,19 +55,19 @@ public class Test1 {
         //DrawCard, Move, PlayCard buttons
         frame.add(bottomPanel);
         upperPanel.add(jl);
-       
-        jl.add(panel1);
-        jl.add(panel2);
-        jl.add(panel3);
-        //Niru
-        panel1.setOpaque(false);
-        panel1.setBounds(0, 1370, 1750, 1420);
-        panel1.add(label1);
-        //kevin
-        panel2.setOpaque(false);
-        panel2.setBounds(0, 1420, 1750, 1420);
-        panel2.add(label2);
-        //Dee
+        
+		jl.add(panel1);
+		jl.add(panel2);
+		jl.add(panel3);
+		//Niru
+		panel1.setOpaque(false);
+		panel1.setBounds(0, 1370, 1750, 1420);
+		panel1.add(label1);
+		//kevin
+		panel2.setOpaque(false);
+		panel2.setBounds(0, 1420, 1750, 1420);
+		panel2.add(label2);
+		//Dee
        panel3.setOpaque(false);
        panel3.setBounds(0, 1470, 1750, 1420);
        panel3.add(label3);
@@ -69,40 +75,44 @@ public class Test1 {
        JScrollPane scrollPane = new JScrollPane(upperPanel);
        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-       scrollPane.setBounds(0, 0, 1280, 500);
+       scrollPane.setBounds(0, 0, 1440, 560);
       
-       drawCardPanel.setPreferredSize(new Dimension(10,10));
-       drawCardPanel.setBackground(Color.black);
-       drawCardPanel.add(drawCardButton);
-       bottomPanel.add(drawCardPanel);
-
-       moveCardPanel.add(moveButton);
-       bottomPanel.add(moveCardPanel);
+       buttonPanel.setPreferredSize(new Dimension(22,22));
+       buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
        
-       playCardPanel.add(playCardButton);
-       bottomPanel.add(playCardPanel);
+       buttonPanel.add(drawCardButton);
+       buttonPanel.add(moveButton);
+       buttonPanel.add(playCardButton);
+      
+     //  JListPanel.setBounds(10, 200, 200, 200);
+       moveList.setBounds(10, 10, 10, 10);
+       JListPanel.add(moveList);
+      
+   //    card1Label.setBounds(100, 500, 100, 100);
+      cardPanel.add(card1Label);
+      
+      
+     //  bottomPanel.add(table);
        
-      // bottomPanel.add(table);
-       
-       JScrollPane buttonPane1 = new JScrollPane(drawCardPanel);
-       buttonPane1.setBounds(10, 500, 150, 45);
+       JScrollPane buttonPane1 = new JScrollPane(buttonPanel);
+       buttonPane1.setBounds(10, 565, 107, 87);
        buttonPane1.setBorder(null);
-     
-       JScrollPane buttonPane2 = new JScrollPane(moveCardPanel);
-       buttonPane2.setBounds(10, 537, 150, 45);
+       
+       JScrollPane buttonPane2 = new JScrollPane(JListPanel);
+       buttonPane2.setBounds (-7, 665, 177, 130);
        buttonPane2.setBorder(null);
        
-       JScrollPane buttonPane3 = new JScrollPane(playCardPanel);
-       buttonPane3.setBounds(10, 573, 150, 45);
+       JScrollPane buttonPane3 = new JScrollPane(cardPanel);
+       buttonPane3.setBounds (200, 565, 217, 300);
        buttonPane3.setBorder(null);
        
        JPanel contentPane = new JPanel(null);
        contentPane.setPreferredSize(new Dimension(1900, 1080));
        contentPane.add(scrollPane);
+       contentPane.add(buttonPane3);
        contentPane.add(buttonPane1);
        contentPane.add(buttonPane2);
-       contentPane.add(buttonPane3);
-       
+  
        frame.setContentPane(contentPane);
        frame.pack();
        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
